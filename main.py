23004b7958043f04a9300f3727d32bb1e16501d9
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-08 18:20:50 -0300

    Feat: Sending email if product's value < 100

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import smtplib
 import requests
 from bs4 import BeautifulSoup
+
+MY_EMAIL = "pythonmailtest2023@gmail.com"
+MY_PASSWORD = "lhsq tscb rqsj rcxa"
 
 url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
 header = {
@@ -13,4 +17,13 @@ soup = BeautifulSoup(response.content, "html.parser")
 price = soup.find(class_="a-offscreen").get_text()
 price_without_currency = price.split("$")[1]
 price_as_float = float(price_without_currency)
-print(price_as_float)
+
+if price_as_float < 100:
+    connection = smtplib.SMTP("smtp.gmail.com")
+    connection.starttls()
+    connection.login(MY_EMAIL, MY_PASSWORD)
+    connection.sendmail(
+        from_addr=MY_EMAIL,
+        to_addrs=MY_EMAIL,
+        msg=f"Churrasqueira controle remoto por apenas R$ {price_as_float}"
+    )
